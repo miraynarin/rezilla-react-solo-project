9de2a7319c1992804b20e28bd481818a3ebb1468
miray <miraynarin@users.noreply.github.com>
2026-02-22 15:55:36 +0300

    hero section scss updated

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -12,15 +12,17 @@ const HeroSection = () => {
     
     <div className={styles.heroFirstSection}>
     <div className={styles.heroContainer}>
+        <div className={styles.leftHeroSection}>
         <div className={styles.leftHeroTop}>
         <p className={styles.leftHeroTitle}>REAL ESTATE</p>
         <h1 className={styles.leftHeroText}>Find a perfect home  <span> you love..!</span></h1>
         <p className={styles.leftHeroAccount}>Etiam eget elementum elit. Aenean dignissim dapibus vestibulum. <span>Integer a dolor eu sapien sodales vulputate ac in purus.</span></p>
-        
-        {/* <div className={styles.leftHeroBottom}> */}
-            <img className={styles.leftHeroImage} src="/images/HeroSection-image.svg" alt="HeroSection Image">
+        </div>
+            <div className={styles.leftHeroImage}>
+            <img  src="/images/herosection-left-image.svg" alt="HeroSection Image">
             </img>
-        {/* </div> */}</div>
+            </div>
+        </div>
         <div className={styles.rightHeroSection}>
         <div className={styles.rightHeroTitle}>
         <button className={styles.rightForSale}>For Sale</button>
