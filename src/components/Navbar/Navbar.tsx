bda6617cbeaa44680c81cdb5c0cb244be69b27eb
miray <miraynarin@users.noreply.github.com>
2026-03-15 22:42:27 +0300

    The SCSS has been adjusted according to the pages' pixel dimensions.

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -11,104 +11,112 @@ const Navbar: React.FC = () => {
 
   return (
     <header className={styles.navbarSection}>
-      <div className={`${styles.linkArea} ${isMenuOpen ? styles.open : ""}`}>
-        <a href="/" className={isActive("/home") ? styles.active : ""}>
-          Home
-        </a>
-        <a href="#" className={isActive("/about") ? styles.active : ""}>
-          About
-        </a>
-        <a href="#" className={isActive("/listings") ? styles.active : ""}>
-          Listings
-        </a>
-        <a href="#" className={isActive("/services") ? styles.active : ""}>
-          Services
-        </a>
-        <a href="#" className={isActive("/blogs") ? styles.active : ""}>
-          Blogs
-        </a>
-      </div>
+      <div className={styles.navbarContainer}>
+        <div className={`${styles.linkArea} ${isMenuOpen ? styles.open : ""}`}>
+          <a href="/" className={isActive("/home") ? styles.active : ""}>
+            Home
+          </a>
+          <a href="#" className={isActive("/about") ? styles.active : ""}>
+            About
+          </a>
+          <a href="#" className={isActive("/listings") ? styles.active : ""}>
+            Listings
+          </a>
+          <a href="#" className={isActive("/services") ? styles.active : ""}>
+            Services
+          </a>
+          <a href="#" className={isActive("/blogs") ? styles.active : ""}>
+            Blogs
+          </a>
+        </div>
 
-      <div className={styles.navbarLogo}>
-        <img
-          className={styles.hauseIcon}
-          src="/images/Navbar/rezilla-house-icon.svg"
-          alt="Rezilla Icon"
-        />
-        <span className={styles.logoText}>Rezilla</span>
-      </div>
-
-      <div className={styles.icon}>
-        <button className={styles.iconButton}>
-          <img src="/images/Navbar/user-icon.svg" alt="User Icon" />
-          Login/Register
-        </button>
-        <button className={styles.houseIconButton}>
+        <div className={styles.navbarLogo}>
           <img
-            className={styles.buttonHouse}
+            className={styles.hauseIcon}
             src="/images/Navbar/rezilla-house-icon.svg"
-            alt="Home Icon"
+            alt="Rezilla Icon"
           />
-          Add Listing
-        </button>
-      </div>
+          <span className={styles.logoText}>Rezilla</span>
+        </div>
 
-      <div className={styles.hamburgerArea}>
-        <button
-          className={`${styles.hamburgerMenu} ${isMenuOpen ? styles.active : ""}`}
-          onClick={toggleMenu}
+        <div className={styles.icon}>
+          <button className={styles.iconButton}>
+            <img src="/images/Navbar/user-icon.svg" alt="User Icon" />
+            Login/Register
+          </button>
+          <button className={styles.houseIconButton}>
+            <img
+              className={styles.buttonHouse}
+              src="/images/Navbar/rezilla-house-icon.svg"
+              alt="Home Icon"
+            />
+            Add Listing
+          </button>
+        </div>
+
+        <div className={styles.hamburgerArea}>
+          <button
+            className={`${styles.hamburgerMenu} ${isMenuOpen ? styles.active : ""}`}
+            onClick={toggleMenu}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+
+        <div
+          className={`${styles.mobileMenuSection} ${isMenuOpen ? styles.open : ""}`}
         >
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </div>
+          <div className={styles.mobileMenu}>
+            <div className={styles.hamburgerArea}>
+              <button
+                className={`${styles.hamburgerMenu} ${isMenuOpen ? styles.active : ""}`}
+                onClick={toggleMenu}
+              >
+                <span></span>
+                <span></span>
+                <span></span>
+              </button>
+            </div>
+            <div className={styles.mobileLinks}>
+              <a href="/" className={isActive("/home") ? styles.active : ""}>
+                Home
+              </a>
+              <a href="#" className={isActive("/about") ? styles.active : ""}>
+                About
+              </a>
+              <a
+                href="#"
+                className={isActive("/listings") ? styles.active : ""}
+              >
+                Listings
+              </a>
+              <a
+                href="#"
+                className={isActive("/services") ? styles.active : ""}
+              >
+                Services
+              </a>
+              <a href="#" className={isActive("/blogs") ? styles.active : ""}>
+                Blogs
+              </a>
+            </div>
 
-      <div
-        className={`${styles.mobileMenuSection} ${isMenuOpen ? styles.open : ""}`}
-      >
-        <div className={styles.mobileMenu}>
-          <div className={styles.hamburgerArea}>
-            <button
-              className={`${styles.hamburgerMenu} ${isMenuOpen ? styles.active : ""}`}
-              onClick={toggleMenu}
-            >
-              <span></span>
-              <span></span>
-              <span></span>
-            </button>
-          </div>
-          <div className={styles.mobileLinks}>
-            <a href="/" className={isActive("/home") ? styles.active : ""}>
-              Home
-            </a>
-            <a href="#" className={isActive("/about") ? styles.active : ""}>
-              About
-            </a>
-            <a href="#" className={isActive("/listings") ? styles.active : ""}>
-              Listings
-            </a>
-            <a href="#" className={isActive("/services") ? styles.active : ""}>
-              Services
-            </a>
-            <a href="#" className={isActive("/blogs") ? styles.active : ""}>
-              Blogs
-            </a>
-          </div>
-
-          <div className={styles.iconMobile}>
-            <button className={styles.iconButton}>
-              <img src="/images/Navbar/user-icon.svg" alt="User Icon" />
-              Login/Register
-            </button>
-            <button className={styles.houseIconButton}>
-              <img
-                className={styles.buttonHouse}
-                src="/images/Navbar/rezilla-house-icon.svg"
-                alt="Home Icon"
-              />
-              Add Listing
-            </button>
+            <div className={styles.iconMobile}>
+              <button className={styles.iconButton}>
+                <img src="/images/Navbar/user-icon.svg" alt="User Icon" />
+                Login/Register
+              </button>
+              <button className={styles.houseIconButton}>
+                <img
+                  className={styles.buttonHouse}
+                  src="/images/Navbar/rezilla-house-icon.svg"
+                  alt="Home Icon"
+                />
+                Add Listing
+              </button>
+            </div>
           </div>
         </div>
       </div>
