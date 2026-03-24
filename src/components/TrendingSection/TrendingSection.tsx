7ea29e1c9433cf3542af612931f7577dec4e04f5
miray <miraynarin@users.noreply.github.com>
2026-03-24 23:03:46 +0300

    TrendingBlog.ts added

--- src/components/TrendingSection/TrendingSection.tsx
+++ src/components/TrendingSection/TrendingSection.tsx
@@ -1,5 +1,18 @@
-const TrendingSection = () => {
-  return <div>TrendingSection</div>;
-};
+import styles from "./TrendingSection.module.scss";
+import TrendingBlog from "./TrendingBlog";
+import { blogs } from "./blogs";
 
-export default TrendingSection;
+export default function TrendingSection() {
+  return (
+    <section className={styles.section}>
+      <p className={styles.subtitle}>WHAT’S TRENDING</p>
+      <h2 className={styles.title}>Latest Blogs & Posts</h2>
+
+      <div className={styles.grid}>
+        {blogs.map((blog) => (
+          <TrendingBlog key={blog.id} blog={blog} />
+        ))}
+      </div>
+    </section>
+  );
+}
