5310f2b38852efec34025c0edd066d526bf315a6
miray <miraynarin@users.noreply.github.com>
2026-04-14 12:45:08 +0300

    RegisterNow updated

--- src/components/RegisterNow/RegisterNow.tsx
+++ src/components/RegisterNow/RegisterNow.tsx
@@ -1,10 +1,41 @@
 import styles from "./RegisterNow.module.scss";
-const RegisterNow = () => {
+import { useState } from "react";
+
+import { FaEye, FaEyeSlash } from "react-icons/fa";
+
+type Props = {
+  onClose: () => void;
+};
+
+const RegisterNow = ({ onClose }: Props) => {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
     <div className={styles.registerNowSection}>
       <input className={styles.registerNowText} placeholder="Name :" />
       <input className={styles.registerNowEmail} placeholder="Email :" />
-      <input className={styles.registerNowPassword} placeholder="Password :" />
+
+      <div className={styles.passwordWrapper}>
+        <input
+          className={styles.registerNowPassword}
+          placeholder="Password :"
+          type={showPassword ? "text" : "password"}
+        />
+
+        <button
+          type="button"
+          className={styles.eyeButton}
+          onClick={() => setShowPassword((prev) => !prev)}
+        >
+          {showPassword ? <FaEyeSlash /> : <FaEye />}
+        </button>
+      </div>
+
+      <button className={styles.registerNowButton}>Register</button>
+
+      <button className={styles.registerNowClose} onClick={onClose}>
+        Close
+      </button>
     </div>
   );
 };
